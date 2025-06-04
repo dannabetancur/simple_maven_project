35b5e97ccc1c9cb485dc187025bf7f893283aafa
dannabetancur <dannarojasb221@gmail.com>
2025-06-03 19:02:14 -0500

    Test

--- src/test/java/edu/unac/CalculatorTest.java
+++ src/test/java/edu/unac/CalculatorTest.java
@@ -5,4 +5,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+
+    @Test
+    void testAddition() {
+        Calculator calculator = new Calculator();
+        assertEquals(4, calculator.add(2, 2));
+    }
+    @Test
+    void testMultiplication() {
+        Calculator calculator = new Calculator();
+        assertEquals(4, calculator.multiply(2, 2));
+    }
 }
